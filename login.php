2f24785c3e46b7362e9297828aec30f727a36038
Victor KANNEMACHER <vkannema@e2r4p3.42.fr>
2017-04-08 18:41:37 +0200

    cleaned files

--- login.php
+++ login.php
@@ -2,14 +2,14 @@
 include("auth.php");
 if ($_POST['login'] && $_POST['passwd'])
 {
-if (auth($_POST['login'], $_POST['passwd']) == TRUE)
-{
-	session_start();
-	$_SESSION['loggued_on_user'] = $_POST['login'];
-	header('Location: index.php');
-}
-else
-	$error = "Wrong id or password";
+	if (auth($_POST['login'], $_POST['passwd']) == TRUE)
+	{
+		session_start();
+		$_SESSION['loggued_on_user'] = $_POST['login'];
+		header('Location: index.php');
+	}
+	else
+		$error = "Wrong id or password";
 }
 else
 	echo "ERROR2\n";
